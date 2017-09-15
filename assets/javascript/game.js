d52fb81e450a5e85f5e4bd077efcabc90b88acce
Max Wang <maxwellswang@gmail.com>
2017-09-14 20:49:50 -0400

    fixed bugs, basically done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 var wins = 0;
 var loses = 0;
 var total = 0;
+var randomNumbers = new Object();
 
 	// generate random numbers for 4 crystals between 1 and 12
 	var rand = function() {
@@ -10,54 +11,53 @@ var total = 0;
 	var randomBigNumber = function() {
 		return Math.ceil(Math.random()* 101 + 19);
 	};
-	var generator = function() {
-		var randomNumbers = {
-		// generate a random number between 19 and 120
-		randomNumber: randomBigNumber(),
-		"crystal1": rand(),
-		"crystal2": rand(),
-		"crystal3": rand(),
-		"crystal4": rand(),
-		};
-		$("#randomNumber").text(randomNumbers.randomNumber);
-	};
 	var reset = function() {
 		total = 0;
 		$("#total").text(total);
 		generator();
 	};
+	var generator = function() {
+		randomNumbers = {
+			// generate a random number between 19 and 120
+			randomNumber: randomBigNumber(),
+			"crystal1": rand(),
+			"crystal2": rand(),
+			"crystal3": rand(),
+			"crystal4": rand(),
+		};
+		$("#randomNumber").text(randomNumbers.randomNumber);
+		console.log(randomNumbers.randomNumber);
+		console.log(randomNumbers.crystal1);
+		console.log(randomNumbers.crystal2);
+		console.log(randomNumbers.crystal3);
+		console.log(randomNumbers.crystal4);
+	};
+
+
 	var checker = function() {
 		// if the total equals the first random number, you win
-		if (total == generator.randomNumbers.randomNumber) {
+		if (total == randomNumbers.randomNumber) {
 			console.log("win");
 			wins += 1;
 			$("#wins").text(wins);
-			$("#randomNumber").text(generator.randomNumbers.randomNumber);
 			reset();
 		// if the total goes above the first random number, you lose
-		} else if (total > generator.randomNumbers.randomNumber) {
+		} else if (total > randomNumbers.randomNumber) {
 			console.log("lose");
 			loses += 1;
 			$("#loses").text(loses);
-			$("#randomNumber").text(generator.randomNumbers.randomNumber);
 			reset();
 		}
 	};
 
-	reset();
-
-	// add the crystal number clicked to a total
 	$("#crystal").on("click", "div", function() {
 		var crystalClick = $(this).attr('id');
-		$("#total").text(total += generator.randomNumbers[crystalClick]);
+		// add the crystal number clicked to a total
+		$("#total").text(total += randomNumbers[crystalClick]);
 		checker();
 	});
 
-	// console.log(randomNumbers.randomNumber);
-	// console.log(randomNumbers.crystal1);
-	// console.log(randomNumbers.crystal2);
-	// console.log(randomNumbers.crystal3);
-	// console.log(randomNumbers.crystal4);
+	reset();
 
 
 });
